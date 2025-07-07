f030b192a826787b84059a178c3f8ca1bba7364d
adbi-hk <biaddiaelhak@gmail.com>
2025-07-07 23:40:13 +0200

    1:53 auth functionality

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,14 +1,57 @@
-import { Header } from "components";
-
+import { Header, StatsCard, TripCard } from "components";
+import { start } from "repl";
+import { dashboardstats, user, allTrips } from "~/constants";
+const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } =
+  dashboardstats;
 const dashboard = () => {
-  const user = { name: "dia" };
   return (
     <main className="dashboard wrapper">
       <Header
         title={`welcome ${user?.name ?? "Guest"} !!`}
         description="track destinations and plan trips in real time "
       />
-      dashboard page content
+      <section className="flex flex-col gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
+          <StatsCard
+            headerTitle="Total Users"
+            total={totalUsers}
+            currentMonthCount={usersJoined.currentMonth}
+            lastMonthCount={usersJoined.lastMonth}
+          />
+
+          <StatsCard
+            headerTitle="Total Trips"
+            total={totalTrips}
+            currentMonthCount={tripsCreated.currentMonth}
+            lastMonthCount={tripsCreated.lastMonth}
+          />
+
+          <StatsCard
+            headerTitle="Active users Today"
+            total={userRole.total}
+            currentMonthCount={userRole.currentMonth}
+            lastMonthCount={userRole.lastMonth}
+          />
+        </div>
+      </section>
+      <section className="container">
+        <h1 className="text-xl font-semibold text-dark-100">Created trips</h1>
+        <div className="trip-grid">
+          {allTrips
+            .slice(0, 4)
+            .map(({ id, name, imageUrls, itinerary, tags, estimatedPrice }) => (
+              <TripCard
+                key={id}
+                id={id.toString()}
+                name={name}
+                imageUrl={imageUrls[0]}
+                location={itinerary?.[0]?.location ?? ""}
+                tags={tags}
+                price={estimatedPrice}
+              />
+            ))}
+        </div>
+      </section>
     </main>
   );
 };
--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,6 @@
 export {default as NavItems} from'./NavItems';
 export {default as Mobilesidebar} from'./MobileSidebar';
 export {default as Header} from'./header';
+export {default as StatsCard} from'./statsCard';
+export {default as TripCard} from'./TripCard';
 
